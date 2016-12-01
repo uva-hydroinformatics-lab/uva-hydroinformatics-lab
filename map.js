34363e6245443405f7cb26fe04adb2e3ad238dce
davjr96 <davjr96@gmail.com>
2016-12-01 10:57:02 -0500

    Initial Commit

---
+++ map.js
@@ -0,0 +1,19 @@
+ var map;
+
+ function initialize(location) {
+     var mapOptions = {}
+
+     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+     var flooded = new google.maps.KmlLayer({
+         url: 'https://raw.githubusercontent.com/Danny-hydro/Danny-hydro/master/bridges.kml',
+         map: map
+     });
+     var boundary = new google.maps.KmlLayer({
+         url: 'https://raw.githubusercontent.com/Danny-hydro/Danny-hydro/master/Boundary.kml',
+         preserveViewport: false,
+         map: map
+     });
+ }
+
+
+ google.maps.event.addDomListener(window, 'load', initialize);
