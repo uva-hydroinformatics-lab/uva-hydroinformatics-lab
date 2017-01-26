ec61a059c2ab0ad7b987270ac49f2860b21bf79e
davjr96 <davjr96@gmail.com>
2017-01-26 09:52:04 -0500

    remove bridges to debug

--- map.js
+++ map.js
@@ -4,10 +4,10 @@
      var mapOptions = {}
 
      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-     var flooded = new google.maps.KmlLayer({
-         url: 'https://raw.githubusercontent.com/Danny-hydro/Danny-hydro/master/bridges.kml',
-         map: map
-     });
+    //  var flooded = new google.maps.KmlLayer({
+    //      url: 'https://raw.githubusercontent.com/Danny-hydro/Danny-hydro/master/bridges.kml',
+    //      map: map
+    //  });
      var boundary = new google.maps.KmlLayer({
          url: 'https://raw.githubusercontent.com/Danny-hydro/Danny-hydro/master/Boundary.kml',
          preserveViewport: false,
